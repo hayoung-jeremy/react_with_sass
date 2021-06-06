ba0ce01900acc1e67523e4a87950fd1c139ef540
Ha young, Kim <jeremiah91@naver.com>
2021-06-07 08:07:58 +0900

    button color

--- src/App.js
+++ src/App.js
@@ -9,6 +9,24 @@ function App() {
         <Button>Click me</Button>
         <Button size="small">Click me</Button>
       </div>
+      <div className="buttons">
+        <Button size="large" color="gray">
+          Click me
+        </Button>
+        <Button color="gray">Click me</Button>
+        <Button size="small" color="gray">
+          Click me
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue">
+          Click me
+        </Button>
+        <Button color="blue">Click me</Button>
+        <Button size="small" color="blue">
+          Click me
+        </Button>
+      </div>
     </div>
   );
 }
