2f8dda71c9c8a8a8beb5c8e790e12c1f7397926d
Ha young, Kim <jeremiah91@naver.com>
2021-06-07 08:23:26 +0900

    more : ...rest

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,12 @@ import React from "react";
 import classNames from "classnames";
 import "./Button.scss";
 
-const Button = ({ children, size, color, outline, fullWidth }) => {
-  return <button className={classNames("Button", size, color, { outline, fullWidth })}>{children}</button>;
+const Button = ({ children, size, color, outline, fullWidth, ...rest }) => {
+  return (
+    <button className={classNames("Button", size, color, { outline, fullWidth })} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 Button.defaultProps = {
