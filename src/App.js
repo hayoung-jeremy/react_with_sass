000df4f4ab7041e49527ef3efcc3590d11e27ada
Ha young, Kim <jeremiah91@naver.com>
2021-06-08 08:04:29 +0900

    outline, size, fullWidth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { ThemeProvider } from "styled-components";
 import "./App.scss";
 import Box from "./components/Box";
 import Button from "./components/Button";
@@ -11,79 +12,141 @@ function App() {
     setCheck(e.target.checked);
   };
   return (
-    <div className="App">
-      <section>
-        <h2>1) buttons</h2>
-        <div className="container">
-          <Button size="large" onClick={() => console.log("clicked!")}>
-            Click me
-          </Button>
-          <Button>Click me</Button>
-          <Button size="small">Click me</Button>
-        </div>
-        <div className="container">
-          <Button size="large" color="gray">
-            Click me
-          </Button>
-          <Button color="gray">Click me</Button>
-          <Button size="small" color="gray">
-            Click me
-          </Button>
-        </div>
-        <div className="container">
-          <Button size="large" color="blue">
-            Click me
-          </Button>
-          <Button color="blue">Click me</Button>
-          <Button size="small" color="blue">
-            Click me
-          </Button>
-        </div>
-        <div className="container">
-          <Button size="large" color="blue" outline>
-            Click me
-          </Button>
-          <Button outline>Click me</Button>
-          <Button size="small" color="gray" outline>
-            Click me
-          </Button>
-        </div>
-        <div className="container">
-          <Button size="large" color="blue" fullWidth>
-            Click me
-          </Button>
-          <Button size="large" fullWidth>
-            Click me
-          </Button>
-          <Button size="large" color="gray" fullWidth>
-            Click me
-          </Button>
-        </div>
-      </section>
+    <ThemeProvider
+      theme={{
+        palette: {
+          blue: "#228be6",
+          pink: "#ffadad",
+          gray: "#444",
+        },
+      }}
+    >
+      <div className="App">
+        <section>
+          <h2>1) buttons</h2>
+          <div className="container">
+            <Button size="large" onClick={() => console.log("clicked!")}>
+              Click me
+            </Button>
+            <Button>Click me</Button>
+            <Button size="small">Click me</Button>
+          </div>
+          <div className="container">
+            <Button size="large" color="gray">
+              Click me
+            </Button>
+            <Button color="gray">Click me</Button>
+            <Button size="small" color="gray">
+              Click me
+            </Button>
+          </div>
+          <div className="container">
+            <Button size="large" color="blue">
+              Click me
+            </Button>
+            <Button color="blue">Click me</Button>
+            <Button size="small" color="blue">
+              Click me
+            </Button>
+          </div>
+          <div className="container">
+            <Button size="large" color="blue" outline>
+              Click me
+            </Button>
+            <Button outline>Click me</Button>
+            <Button size="small" color="gray" outline>
+              Click me
+            </Button>
+          </div>
+          <div className="container">
+            <Button size="large" color="blue" fullWidth>
+              Click me
+            </Button>
+            <Button size="large" fullWidth>
+              Click me
+            </Button>
+            <Button size="large" color="gray" fullWidth>
+              Click me
+            </Button>
+          </div>
+        </section>
 
-      <section>
-        <h2>2) Css Module</h2>
-        <div className="container">
-          <Box></Box>
-        </div>
-        <div className="container">
-          <CheckBox onChange={onChange} checked={check}>
-            다음 약관에 모두 동의
-          </CheckBox>
-          <p>
-            <b>Check : </b>
-            {check ? "true" : "false"}
-          </p>
-        </div>
-      </section>
+        <section>
+          <h2>2) Css Module</h2>
+          <div className="container">
+            <Box></Box>
+          </div>
+          <div className="container">
+            <CheckBox onChange={onChange} checked={check}>
+              다음 약관에 모두 동의
+            </CheckBox>
+            <p>
+              <b>Check : </b>
+              {check ? "true" : "false"}
+            </p>
+          </div>
+        </section>
 
-      <section>
-        <h2>3) Styled Components</h2>
-        <div className="container">
-          <StyledButton>BUTTON</StyledButton>
-        </div>
-      </section>
-    </div>
+        <section>
+          <h2>3) Styled Components</h2>
+          <div className="container">
+            <StyledButton color="blue" size="large">
+              BUTTON
+            </StyledButton>
+            <StyledButton color="blue" size="medium">
+              BUTTON
+            </StyledButton>
+            <StyledButton color="blue" size="small">
+              BUTTON
+            </StyledButton>
+          </div>
+          <div className="container">
+            <StyledButton color="pink" size="large">
+              BUTTON
+            </StyledButton>
+            <StyledButton color="pink" size="medium">
+              BUTTON
+            </StyledButton>
+            <StyledButton color="pink" size="small">
+              BUTTON
+            </StyledButton>
+          </div>
+          <div className="container">
+            <StyledButton color="gray" size="large">
+              BUTTON
+            </StyledButton>
+            <StyledButton color="gray" size="medium">
+              BUTTON
+            </StyledButton>
+            <StyledButton color="gray" size="small">
+              BUTTON
+            </StyledButton>
+          </div>
+          <div className="container">
+            <StyledButton color="blue" size="large" outline>
+              BUTTON
+            </StyledButton>
+            <StyledButton color="pink" size="medium" outline>
+              BUTTON
+            </StyledButton>
+            <StyledButton color="gray" size="small" outline>
+              BUTTON
+            </StyledButton>
+          </div>
+          <div className="container">
+            <StyledButton color="blue" size="large" fullWidthStyle>
+              BUTTON
+            </StyledButton>
+            <StyledButton color="pink" size="large" fullWidthStyle>
+              BUTTON
+            </StyledButton>
+            <StyledButton color="gray" size="large" fullWidthStyle>
+              BUTTON
+            </StyledButton>
+          </div>
+        </section>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/StyledButton.js
+++ src/components/StyledButton.js
@@ -1,6 +1,69 @@
-import { darken, lighten } from "polished";
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { darken, lighten } from "polished";
+
+const colorStyles = css`
+  // color
+  ${({ theme, color }) => {
+    const selected = theme.palette[color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.05, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+
+      // outline
+      ${(props) =>
+        props.outline &&
+        css`
+          color: ${selected};
+          background: none;
+          border: 1px solid ${selected};
+          &:hover {
+            background: ${selected};
+            color: white;
+          }
+        `}
+    `;
+  }}
+`;
+
+const sizes = {
+  large: {
+    height: "3rem",
+    fontSize: "1.25rem",
+  },
+  medium: {
+    height: "2.25rem",
+    fontSize: "1rem",
+  },
+  small: {
+    height: "1.75rem",
+    fontSize: "0.875rem",
+  },
+};
+
+const sizeStyles = css`
+  ${({ size }) => css`
+    height: ${sizes[size].height};
+    font-size: ${sizes[size].fontSize};
+  `}
+`;
+
+const fullWidthStyle = css`
+  ${(props) =>
+    props.fullWidthStyle &&
+    css`
+      width: 100%;
+      &:not(:first-child) {
+        margin-left: 0;
+        margin-top: 0.75rem;
+      }
+    `}
+`;
 
 const StyledBtn = styled.button`
   // common
@@ -17,26 +80,31 @@ const StyledBtn = styled.button`
   padding-left: 1rem;
   padding-right: 1rem;
 
-  // size
-  height: 2.25rem;
-  font-size: 1rem;
-
   // color
-  background: #228be6;
-  &:hover {
-    background: ${lighten(0.1, "#228be6")};
-  }
-  &:active {
-    background: ${darken(0.1, "#228be6")};
+  ${colorStyles}
+
+  // size
+  ${sizeStyles}
+  
+  &:not(:first-child) {
+    margin-left: 0.75rem;
   }
 
-  & + & {
-    margin-left: 1rem;
-  }
+  // fullWidthStyle
+  ${fullWidthStyle}
 `;
 
-const StyledButton = ({ children, ...rest }) => {
-  return <StyledBtn {...rest}>{children}</StyledBtn>;
+const StyledButton = ({ children, color, size, outline, fullWidthStyle, ...rest }) => {
+  return (
+    <StyledBtn color={color} size={size} outline={outline} fullWidthStyle={fullWidthStyle} {...rest}>
+      {children}
+    </StyledBtn>
+  );
+};
+
+StyledButton.defaultProps = {
+  color: "pink",
+  size: "medium",
 };
 
 export default StyledButton;
